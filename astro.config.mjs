c259580ed300547a31c66ac42f520591d8eddb0a
mosaleh52 <mosaleh@mail.com>
2024-07-22 14:50:30 +0300

    font style update with sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
 import lqip from "vite-plugin-lqip";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mahran-alsarea.com",
   vite: {
-    plugins: [lqip()],
+    plugins: [lqip()]
   },
-
-  integrations: [icon()],
+  integrations: [icon(), sitemap(), robotsTxt()]
 });
